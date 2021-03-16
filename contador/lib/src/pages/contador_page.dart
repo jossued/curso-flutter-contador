83388b2af90244d8cf089b7d33675cb084f0d892
Jossué Dután <jossuedutan@gmail.com>
2021-03-16 00:38:52 -0500

    ordenar botones

--- contador/lib/src/pages/contador_page.dart
+++ contador/lib/src/pages/contador_page.dart
@@ -13,34 +13,61 @@ class _ContadorPageState extends State<ContadorPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Título'),
-        centerTitle: true,
-      ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Número de clics:',
-            style: _estiloTexto,
-          ),
-          Text(
-            '$_conteo',
-            style: _estiloTexto,
-          )
-        ],
-      )),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          print('botón presionado');
-          setState(() {
-            _conteo++;
-          });
-        },
-      ),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.miniStartFloat,
+        appBar: AppBar(
+          title: Text('Título'),
+          centerTitle: true,
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Número de clics:',
+              style: _estiloTexto,
+            ),
+            Text(
+              '$_conteo',
+              style: _estiloTexto,
+            )
+          ],
+        )),
+        floatingActionButton: _crearBotones()
+        // floatingActionButtonLocation: FloatingActionButtonLocation.miniStartFloat,
+        );
+  }
+
+  Widget _crearBotones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        SizedBox(width: 30.00,),
+        FloatingActionButton(
+          child: Icon(Icons.exposure_zero),
+          onPressed: () {
+            setState(() {
+              _conteo++;
+            });
+          },
+        ),
+        Expanded(child: SizedBox()),
+        FloatingActionButton(
+          child: Icon(Icons.remove),
+          onPressed: () {
+            setState(() {
+              _conteo++;
+            });
+          },
+        ),
+        SizedBox(width: 5.00,),
+        FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              _conteo++;
+            });
+          },
+        ),
+      ],
     );
   }
 }
